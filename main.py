ae7d1bdd860eb7ed2e0c7712e97089006289ffe3
GuhhSec <87870941+Teguh1223@users.noreply.github.com>
2022-03-25 20:54:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ def wibu():
 		    print("Sent File To Ip and Port Connecting Easy Down!!!")
 		    
 for y in range(threads):
-    th = threading.Thread(target = Set)
+    th = threading.Thread(target = Sent)
     th.start()
